a1a67c6d7c1415b58b7330d3977377d55a036538
yanakiv30 <yanakiv30@gmail.com>
2023-12-22 17:59:24 +0200

    only-one-open

--- src/App.js
+++ src/App.js
@@ -25,20 +25,29 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem 
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title={el.title} 
+        text={el.text} 
+        num={i} 
+        key={el.title} />
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, text, curOpen, onOpen }) {
+
+const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen(isOpen => !isOpen);
+    onOpen(num);
   }
 
   return (
